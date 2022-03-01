e9942f9e4c53b388f5cbb3dde6c5b1d98be7b7b6
Francisntahimpera <ntahimperafrancis@gmail.com>
2022-03-01 14:21:53 +0100

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h3>{{ $route.params.slug }} </h3>
   </div>
 </template>
+<script>
+import {useRoute} from "vue-router";
+import {reactive, toRefs } from "vue";
+
+
+export default {
+  
+  setup(){
+      const route = useRoute();
+      const state = reactive({
+        pokemon:null
+      })
+
+      fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}` )
+       .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.pokemon = data;
+         
+          })
+          return{...toRefs(state)}
+      
+      
+}}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex justify-center w-full">
+    <input type="text" class="p-2 mt-10 border-2 border-blue-500" placeholder="enter poke name" 
+      v-model="text"/>
   </div>
+  <div class="flex flex-wrap justify-center p-4 mt-10">
+    <div
+      class="ml-4 text-blue-500 text-2x"
+      v-for="(pokemon, idx) in filteredPokemon"
+      :key="idx"
+    >
+    <router-link :to="`/about/${urlIdLookup[pokemon.name]}`" >{{ pokemon.name }}</router-link>
+     </div>
+  </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { computed, reactive, toRefs } from "vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "HomeView",
+  
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {}, 
+      text:"",
+      filteredPokemon:computed( ()=> updatePokemon() )
+    })
+    
+    function updatePokemon(){
+      if(!state.text){
+        return []
+      }
+      
+      return state.pokemons.filter((pokemon)=> pokemon.name.includes(state.text))
+      
+    }
+    
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0/")
+    
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.pokemons = data.results;
+        state.urlIdLookup.data.results.reduce((acc, cur, idx) =>
+         acc = {...acc, [cur.name]:idx+1},
+          {})
+      
+      })
+      
+    return { ...toRefs(state) };
   }
 }
 </script>
